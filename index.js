c22bbd7ab5d211f63573fde187f5a8f328e40768
Scott Elcomb <scott@humancode.ca>
2016-11-11 21:15:42 -0500

    wip: bring in socket.io, bootstrap, jquery

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello from Node Knockout 2016!')
-})
+//app.get('/', function(request, response) { response.send('Hello from Node Knockout 2016!'); });
+
+/* --------- disable fixed --------- */
+/*
+app.get('/', function(req, res){ res.sendfile('public/index.html'); });
+app.get('/css/client.css', function(req, res){ res.sendfile('public/client.css'); });
+app.get('/js/bundle.js', function(req, res){ res.sendfile('public/js/bundle.js'); });
+app.get('/js/bundle.min.js', function(req, res){ res.sendfile('public/js/bundle.min.js'); });
+*/
+/* --------- disable fixed --------- */
+
+io.on('connection', function(socket) {
+    console.log('connection!');
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
---
+++ public/js/client.js
@@ -0,0 +1,62 @@
+window.loggerLevel = 3; // max log level, will display logging messages at this
+                        // level or below, see log() for more details
+
+/* log(string [, object]);
+ *
+ * optional object should provide an integer "level" that should be one of:
+ *      1: log (default)
+ *      2: warn
+ *      3: error
+*/
+window.log = function(msg, opts) {
+    opts = opts || {};
+    opts.level = opts.level || 1;
+
+    var logFn = 'log';
+
+    switch(opts.level) {
+        case 2:
+            logFn = 'warn';
+            break;
+
+        case 3:
+            logFn = 'error';
+            break;
+
+        case 1:
+        default:
+            logFn = 'log';
+    }
+
+    if (opts.level <= loggerLevel) {
+        if (typeof console[logFn] === 'function') {
+            console[logFn](msg);
+        }
+    }
+}
+
+window.addEventListener('load', function() {
+    log('loaded', { level: 1 });
+
+
+    // --- socket.io tests ---
+    // window.socket = require('socket.io-client')('http://localhost:5000');
+    window.socket = require('../../node_modules/socket.io-client/socket.io.js')('http://localhost:5000');
+
+    socket.on('connect', function(){ log('connected to socket server'); });
+    socket.on('event', function(data){ log('server event caught'); console.log(data); });
+    socket.on('disconnect', function(){ log('disconnected from socket server'); });
+
+
+    // --- jquery, bootstrap tests ---
+    window.jQuery = $ = require('jquery');
+
+    var bootstrap = require('bootstrap/dist/js/bootstrap');
+    var style = require('../../node_modules/bootstrap/dist/css/bootstrap.css');
+
+    var popover = document.createElement('span');
+    popover.innerHTML = 'I have a popover';
+    document.body.appendChild(popover);
+
+    $(popover).popover({ content: 'I am popover text' });
+});
